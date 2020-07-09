50a6619d9c770e31030fa31287089bc7d71af16e
David Herman <bit.spittle@gmail.com>
2020-07-08 17:15:55 -0700

    Experimental snapshot

--- common/CircularBuffer.gd
+++ common/CircularBuffer.gd
@@ -28,26 +28,49 @@ class Iterator:
 	func _iter_get(arg):
 		return _values[_curr]
 
-var _max_size = 0
+var _capacity = 0
 var _values = []
 var _first = 0
 var _last = -1
 
-func _init(max_size: int = 100):
-	_max_size = max_size
+func _init(capacity: int = 100):
+	_capacity = capacity
 
 func iter() -> Iterator:
 	return Iterator.new(_values, _first)
 
+func capacity() -> int:
+	return _capacity
+
+func size() -> int:
+	return _values.size()
+
+func empty() -> bool:
+	return size() == 0
+
+func get_item(index: int):
+	var size = size()
+	if index >= size || index < -size:
+		push_error("Invalid index for circular buffer get: " + str(index))
+
+	if index < 0:
+		index += size
+
+	return _values[(_first + index) % size]
+
 func append(value) -> void:
 	var values_size = _values.size()
-	if values_size < _max_size:
+	if values_size < _capacity:
 		_values.append(value)
 	else:
 		_values[_first] = value
 		_last = _first
 		_first = (_first + 1) % values_size
 
+func clear() -> void:
+	_values.clear()
+	_first = 0
+	_last = -1
 
 func _to_string() -> String:
 	var result = "["
